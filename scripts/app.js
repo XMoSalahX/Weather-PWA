4369cbe4a92b3faa9935989cf1b9fe97974afec9
XMoSalahX <01154615199>
2022-01-22 21:16:35 +0200

    test

--- scripts/app.js
+++ scripts/app.js
@@ -200,7 +200,7 @@
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
-            .register('../sw.js')
+            .register('../../Weather-PWA/sw.js')
             .then(function() {
                 console.log('Service Worker Registered');
             });
