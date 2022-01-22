88e1eaccf15a9aad63736f0bef813c11e869b587
XMoSalahX <01154615199>
2022-01-22 20:55:07 +0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-// var gulp = require("gulp"),
-//     swPrecache = require("sw-precache");
+var gulp = require("gulp"),
+    swPrecache = require("sw-precache");
 
-// gulp.task("generate-sw", function() {
-//     var swOptions = {
-//         staticFileGlobs: [
-//             './*.html',
-//             './images/**/*.*',
-//             './scripts/*.*',
-//             './styles/*.*'
-//         ],
-//         stripPrefix: '.',
-//         runtimeCaching: [{
-//             urlPattern: /^https:\/\/publicdata-weather\.firebaseio\.com/,
-//             handler: 'networkFirst',
-//             options: {
-//                 cache: {
-//                     name: 'weatherData-v2'
-//                 }
-//             }
-//         }]
-//     };
+gulp.task("generate-sw", function() {
+    var swOptions = {
+        staticFileGlobs: [
+            './*.html',
+            './images/**/*.*',
+            './scripts/*.*',
+            './styles/*.*'
+        ],
+        stripPrefix: '.',
+        runtimeCaching: [{
+            urlPattern: /^https:\/\/publicdata-weather\.firebaseio\.com/,
+            handler: 'networkFirst',
+            options: {
+                cache: {
+                    name: 'weatherData-v3'
+                }
+            }
+        }]
+    };
 
-//     return swPrecache.write("sw.js", swOptions)
-// })
+    return swPrecache.write("sw.js", swOptions)
+})
