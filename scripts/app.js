44b20a01a231892cc2d84c78ea21a5d95a57dd1c
XMoSalahX <01154615199>
2022-01-23 13:20:27 +0200

    test

--- scripts/app.js
+++ scripts/app.js
@@ -17,13 +17,13 @@
         },
         daily: {
             data: [
-                { icon: '../images/clear-day', temperatureMax: 55, temperatureMin: 34 },
-                { icon: '../images/rain', temperatureMax: 55, temperatureMin: 34 },
-                { icon: '../images/snow', temperatureMax: 55, temperatureMin: 34 },
-                { icon: '../images/sleet', temperatureMax: 55, temperatureMin: 34 },
-                { icon: '../images/fog', temperatureMax: 55, temperatureMin: 34 },
-                { icon: '../images/wind', temperatureMax: 55, temperatureMin: 34 },
-                { icon: '../images/partly-cloudy-day', temperatureMax: 55, temperatureMin: 34 }
+                { icon: '../../Weather-PWA/images/clear-day', temperatureMax: 55, temperatureMin: 34 },
+                { icon: '../../Weather-PWA/images/rain', temperatureMax: 55, temperatureMin: 34 },
+                { icon: '../../Weather-PWA/images/snow', temperatureMax: 55, temperatureMin: 34 },
+                { icon: '../../Weather-PWA/images/sleet', temperatureMax: 55, temperatureMin: 34 },
+                { icon: '../../Weather-PWA/images/fog', temperatureMax: 55, temperatureMin: 34 },
+                { icon: '../../Weather-PWA/images/wind', temperatureMax: 55, temperatureMin: 34 },
+                { icon: '../../Weather-PWA/images/partly-cloudy-day', temperatureMax: 55, temperatureMin: 34 }
             ]
         }
     };
